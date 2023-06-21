6e7b16ea59b4818681fe790ce89ec727175d937f
pouria <npouriaa@gmail.com>
2023-06-21 13:54:50 +0430

    setting up search component

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,22 +2,29 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
 const Searchbar = () => {
+  const navigate = useNavigate();
+  const [searchTerms, setSerachTerms] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search/${searchTerms}`);
+  };
+
   return (
-    <form
-      autoComplete="off"
-      className="p-2 text-gray-300"
-    >
+    <form onSubmit={(e) => handleSubmit(e)} autoComplete="off" className="p-2 text-gray-300">
       <label htmlFor="search-filed" className="sr-only">
         Search all songs
       </label>
       <div className="flex flex-row justify-start items-center">
         <FiSearch className="w-5 h-5 ml-4" />
         <input
+        value={searchTerms}
+        onChange={(e) => setSerachTerms(e.target.value)}
           type="text"
           name="search-filed"
           id="search-filed"
           placeholder="search"
-          className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base tecxt-white p-4"
+          className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4"
         />
       </div>
     </form>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,39 @@
-const Search = () => (
-  <div>Search</div>
-);
+import { useSelector } from "react-redux";
+import { Error, Loader, SongCard } from "../components";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+const Search = () => {
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  console.log(data);
+
+  if (isFetching) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-xl text-white text-left mt-4 mb-10">
+        Top Charts
+      </h2>
+      <div className="flex flex-wrap sm:justify-start xl:justify-center gap-8">
+        {data?.tracks.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            i={i}
+            data={data}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Search;
