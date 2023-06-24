39d807a4101537d18beaa0c1aa40a8d279d30533
pouria <npouriaa@gmail.com>
2023-06-25 00:40:11 +0430

    changes

--- src/components/MusicPlayer/Seekbar.jsx
+++ src/components/MusicPlayer/Seekbar.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 const Seekbar = ({ value, min, max, onInput }) => {
-  // converts the time to format 0:00
   const getTime = (time) =>
     `${Math.floor(time / 60)}:${`0${Math.floor(time % 60)}`.slice(-2)}`;
 
   return (
-    <div className="hidden fixed bottom-4 sm:flex flex-row items-center">
+    <div className=" fixed bottom-4 flex flex-row items-center">
       <p className="text-white text-xs">
         {value === 0 ? "0:00" : getTime(value)}
       </p>
@@ -17,7 +16,7 @@ const Seekbar = ({ value, min, max, onInput }) => {
         min={min}
         max={max}
         onInput={onInput}
-        className="md:block cursor-pointer accent-white w-24 md:w-56 2xl:w-[450px] h-1 mx-4 2xl:mx-6 rounded-lg"
+        className=" cursor-pointer accent-[#1FDF64] w-24 md:w-56 2xl:w-[450px] h-1 mx-4 2xl:mx-6 rounded-lg"
       />
       <p className="text-white  text-xs">{max === 0 ? "0:00" : getTime(max)}</p>
     </div>
